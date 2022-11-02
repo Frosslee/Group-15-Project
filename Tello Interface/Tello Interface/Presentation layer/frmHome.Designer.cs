95575f1f6077219c6461d337a312119e3c359701
Frosslee <91604209+Frosslee@users.noreply.github.com>
2022-11-02 13:30:59 +0200

    layered architect

---
+++ Tello Interface/Tello Interface/Presentation layer/frmHome.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Tello_Interface
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbldisco = new System.Windows.Forms.Label();
+            this.lblconnctd = new System.Windows.Forms.Label();
+            this.lblcnt = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnFly = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.rtbPython = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imgpCamFeed = new System.Windows.Forms.PictureBox();
+            this.lblCamera = new System.Windows.Forms.Label();
+            this.cbCamerabox = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgpCamFeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.lbldisco);
+            this.panel1.Controls.Add(this.lblconnctd);
+            this.panel1.Controls.Add(this.lblcnt);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.lblType);
+            this.panel1.Controls.Add(this.lblUser);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1653, 36);
+            this.panel1.TabIndex = 16;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lbldisco
+            // 
+            this.lbldisco.AutoSize = true;
+            this.lbldisco.Location = new System.Drawing.Point(978, 13);
+            this.lbldisco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbldisco.Name = "lbldisco";
+            this.lbldisco.Size = new System.Drawing.Size(90, 16);
+            this.lbldisco.TabIndex = 21;
+            this.lbldisco.Text = "Disconnected";
+            // 
+            // lblconnctd
+            // 
+            this.lblconnctd.AutoSize = true;
+            this.lblconnctd.Location = new System.Drawing.Point(978, 13);
+            this.lblconnctd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblconnctd.Name = "lblconnctd";
+            this.lblconnctd.Size = new System.Drawing.Size(72, 16);
+            this.lblconnctd.TabIndex = 20;
+            this.lblconnctd.Text = "Connected";
+            // 
+            // lblcnt
+            // 
+            this.lblcnt.AutoSize = true;
+            this.lblcnt.Location = new System.Drawing.Point(457, 11);
+            this.lblcnt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcnt.Name = "lblcnt";
+            this.lblcnt.Size = new System.Drawing.Size(83, 16);
+            this.lblcnt.TabIndex = 19;
+            this.lblcnt.Text = "Connecting...";
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(4, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 30);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(300, 5);
+            this.lblType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(92, 24);
+            this.lblType.TabIndex = 4;
+            this.lblType.Text = "UserType";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(97, 5);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(97, 24);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "Username";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(1544, 5);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(36, 16);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(335, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(353, 29);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Tello Command Center";
+            // 
+            // btnFly
+            // 
+            this.btnFly.Location = new System.Drawing.Point(780, 43);
+            this.btnFly.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFly.Name = "btnFly";
+            this.btnFly.Size = new System.Drawing.Size(100, 28);
+            this.btnFly.TabIndex = 18;
+            this.btnFly.Text = "Fly";
+            this.btnFly.UseVisualStyleBackColor = true;
+            this.btnFly.Click += new System.EventHandler(this.btnFly_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(911, 43);
+            this.btnEnd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(100, 28);
+            this.btnEnd.TabIndex = 20;
+            this.btnEnd.Text = "End Flight";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(780, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 28);
+            this.button1.TabIndex = 21;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(737, 144);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 28);
+            this.button3.TabIndex = 22;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(780, 144);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 28);
+            this.button4.TabIndex = 23;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(822, 144);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 28);
+            this.button5.TabIndex = 24;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(737, 198);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 25;
+            this.button6.Text = "Takeoff";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(737, 234);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 28);
+            this.button7.TabIndex = 26;
+            this.button7.Text = "Land";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(737, 269);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 28);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "Flip";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // rtbPython
+            // 
+            this.rtbPython.Location = new System.Drawing.Point(13, 85);
+            this.rtbPython.Margin = new System.Windows.Forms.Padding(4);
+            this.rtbPython.Name = "rtbPython";
+            this.rtbPython.Size = new System.Drawing.Size(708, 481);
+            this.rtbPython.TabIndex = 19;
+            this.rtbPython.Text = "";
+            this.rtbPython.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(1227, 509);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 35;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // imgpCamFeed
+            // 
+            this.imgpCamFeed.Location = new System.Drawing.Point(911, 124);
+            this.imgpCamFeed.Name = "imgpCamFeed";
+            this.imgpCamFeed.Size = new System.Drawing.Size(706, 378);
+            this.imgpCamFeed.TabIndex = 34;
+            this.imgpCamFeed.TabStop = false;
+            this.imgpCamFeed.Click += new System.EventHandler(this.imgpCamFeed_Click);
+            // 
+            // lblCamera
+            // 
+            this.lblCamera.AutoSize = true;
+            this.lblCamera.BackColor = System.Drawing.Color.Transparent;
+            this.lblCamera.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblCamera.Location = new System.Drawing.Point(1073, 85);
+            this.lblCamera.Name = "lblCamera";
+            this.lblCamera.Size = new System.Drawing.Size(55, 16);
+            this.lblCamera.TabIndex = 33;
+            this.lblCamera.Text = "Camera";
+            // 
+            // cbCamerabox
+            // 
+            this.cbCamerabox.FormattingEnabled = true;
+            this.cbCamerabox.Location = new System.Drawing.Point(1146, 82);
+            this.cbCamerabox.Name = "cbCamerabox";
+            this.cbCamerabox.Size = new System.Drawing.Size(321, 24);
+            this.cbCamerabox.TabIndex = 32;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1649, 704);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.imgpCamFeed);
+            this.Controls.Add(this.lblCamera);
+            this.Controls.Add(this.cbCamerabox);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.rtbPython);
+            this.Controls.Add(this.btnFly);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmHome";
+            this.Text = "frmHome";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgpCamFeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFly;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label lblconnctd;
+        private System.Windows.Forms.Label lblcnt;
+        private System.Windows.Forms.Label lbldisco;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox rtbPython;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox imgpCamFeed;
+        private System.Windows.Forms.Label lblCamera;
+        private System.Windows.Forms.ComboBox cbCamerabox;
+    }
+}
---
+++ Tello Interface/Tello Interface/Presentation layer/frmLogin.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Tello_Interface
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1068, 36);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnExit
+            // 
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(4, 2);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(25, 30);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(907, 5);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(36, 16);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date";
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Location = new System.Drawing.Point(227, 47);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(148, 31);
+            this.tbxUsername.TabIndex = 7;
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.Controls.Add(this.label2);
+            this.gbLogin.Controls.Add(this.label1);
+            this.gbLogin.Controls.Add(this.tbxPassword);
+            this.gbLogin.Controls.Add(this.tbxUsername);
+            this.gbLogin.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbLogin.ForeColor = System.Drawing.Color.Lime;
+            this.gbLogin.Location = new System.Drawing.Point(279, 138);
+            this.gbLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Padding = new System.Windows.Forms.Padding(4);
+            this.gbLogin.Size = new System.Drawing.Size(443, 190);
+            this.gbLogin.TabIndex = 8;
+            this.gbLogin.TabStop = false;
+            this.gbLogin.Text = "Login Details:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bodoni MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Turquoise;
+            this.label2.Location = new System.Drawing.Point(40, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bodoni MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Turquoise;
+            this.label1.Location = new System.Drawing.Point(36, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Username:";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(227, 110);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PasswordChar = '*';
+            this.tbxPassword.Size = new System.Drawing.Size(148, 31);
+            this.tbxPassword.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(117, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(767, 63);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tello Command Center";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnLogin.Location = new System.Drawing.Point(432, 357);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(151, 42);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.AutoSize = true;
+            this.lnkRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkRegister.LinkColor = System.Drawing.Color.Lime;
+            this.lnkRegister.Location = new System.Drawing.Point(474, 403);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(63, 18);
+            this.lnkRegister.TabIndex = 11;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Register";
+            this.lnkRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRegister_LinkClicked);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gbLogin);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.LinkLabel lnkRegister;
+    }
+}
